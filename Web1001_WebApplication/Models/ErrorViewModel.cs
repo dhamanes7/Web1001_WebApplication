99e62a26c119669828596c15dd7fd79256473aef
shubhamdhamane <A00257743@mycambrian.ca>
2022-07-21 19:46:52 -0400

    first commit

---
+++ Web1001_WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Web1001_WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
